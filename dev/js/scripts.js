6ad677c17dbedb06d6f2bf8000599819a4411d2c
eco38drexel <81782897+eco38drexel@users.noreply.github.com>
2021-12-06 14:02:55 -0500

    basic progress

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,20 @@
-// import { gsap } from "gsap";
+import { gsap } from "gsap";
+// import { GSDevTools } from "gsap/GSDevTools";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin);
+
+const mainTL = gsap.timeline();
+
+
+function LogoAnimation(){
+    const tl = gsap.timeline();
+
+    tl.from("#stem-five",{scale:0, duration:4, y:"-=4"})
+    tl.from("#leaf-2", {scale:0, duration:4});
+    tl.from("#leaf-3", {scale:0, duration:4});
+    tl.from("#leaf-1", {scale:0, duration:4});
+
+}
+
+mainTL.add(LogoAnimation());
